71f1aab03932847de59ad05940232dcb2fdce66b
horatiu <horatiu.silinca@gmail.com>
2015-08-19 11:05:55 +0300

    test

--- front/src/js/GridController.js
+++ front/src/js/GridController.js
@@ -1,21 +1,35 @@
 "use strict";
 
 var PersonService = require("./PersonService");
-
+var $ = require("jquery");
+var baseUrl = "http://localhost:3000/api";
 init();
 
 function init(){
 	var personService = new PersonService();
 
-	personService.add(
-		{firstName: "Ion",
-		 lastName: "Vasile",
-		 age : 25}, function(){	console.log("Added the person");
-		});
+	//var jsonData = '[{"rank":"9","content":"Alon","UID":"5"},{"rank":"6","content":"Tala","UID":"6"}]';
+	
+	// personService.add(
+	// 	{id:2,
+	// 	firstName: "Ion",
+	// 	 lastName: "Vasile",
+	// 	 age : 25}, function(){	console.log("Added the person");
+	// 	});
 
-	personService.getAll( function(data){ 
-		  console.log(data);
-		
-		});
+	personService.populate();
+	// personService.deleteId();
+
+
+	// personService.editId(
+	// 	{id:1,
+	// 	firstName: "update",
+	// 	 lastName: "update",
+	// 	 age : 10}, function(){	console.log("Update id");
+	// 	});
+
+	// personService.getAll( function(data){ 
+	// 	 console.log(data);
+	// });
 
 }
--- front/src/js/PersonService.js
+++ front/src/js/PersonService.js
@@ -10,7 +10,27 @@ function PersonService () {
 			method: "post",
 			data: data,
 			success: function(data) {
-				callback(data);
+				// callback(data);
+				console.log("added person");
+			}
+		});
+	};
+
+	this.deleteId = function (id){
+		$.ajax(baseUrl + "/people/" + id, {
+			method: "delete",
+			success: function() {
+				console.log("deleted");
+			}
+		});
+	};
+
+	this.editId = function (data){
+		$.ajax(baseUrl + "/people/",{
+			method: "put",
+			data:data,
+			success: function() {
+				console.log("updated");
 			}
 		});
 	};
@@ -24,6 +44,19 @@ function PersonService () {
 			}
 		});
 	};
+	
+	this.populate = function (firstName){
+    $.ajax(baseUrl + "/people/" + firstName , {
+    	method : 'POST',
+    	success: function (response) {
+        	var trHTML = '';
+        	$.each(response, function (i, item) {
+            	trHTML += '<tr><td>' + item.firstName + '</td></tr>';
+        	});
+        	$('#records_table').append(trHTML);
+    
+}});
+};
 
 	//getAll
 
